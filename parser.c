0a7d7356affa1768fbaca28d83a3cbae72569e9a
Jean-philippe Martel <jemartel@c2r4p6.42quebec.com>
2021-03-27 14:39:09 -0400

    weirdstuff

--- parser.c
+++ parser.c
@@ -1,16 +1,69 @@
 #include "fcntl.h"
 #include "unistd.h"
 #include "stdlib.h"
-
+#include "stdio.h"
 
 #define BUFFER 4096
+
+int	ft_str_is_alpha (char *str)
+{
+		if (*str >= 'a' && *str <= 'z')
+			return (1);
+
+		return (0);
+}
+
+int	ft_strlen(char *str)
+{
+	char	*start;
+	int		i;
+
+	start = str;
+	i = 0;
+	while (*start != '\0')
+	{
+		start++;
+		i++;
+	}
+	return (i);
+}
+
+char *ft_strdup(char *src)
+{
+	char	*copy;
+	size_t	size;
+	int	i;
+
+	i = 0  ;
+	size = ft_strlen(src);
+	copy = (char *)malloc(size + 1);
+		while(src[i])
+		{
+			copy[i] = src[i];
+			i++;
+		}
+		copy[i] = '\0';
+
+return copy;
+
+}
+
 int main(void)
 {
-char buffer[BUFFER];
-char *iter;
+char *buffer;
+int size;
+int file_size;
+char **nodumbray;
+file_size = 0;
+size = BUFFER;
+buffer = (char *)malloc(sizeof(char) * 700);
 int file = open("only.txt",O_RDONLY);
-	//should make mover go aroud file until the end?
-	read(file,buffer,BUFFER);
-	iter = buffer;
-	write(1,iter,25);
+nodumbray = (char **)malloc(sizeof(char) * ft_strlen(buffer));
+while(size  == BUFFER)
+{
+file_size += read(file,buffer,BUFFER);
+size = read(file,buffer,BUFFER);
+}
+
+buffer = (char *)malloc(sizeof(char) * file_size + 1);
 }
