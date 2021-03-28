ad55c632054058dd7cc929ac8fe285ec266a543d
Jean-philippe Martel <jemartel@c2r4p6.42quebec.com>
2021-03-28 11:00:07 -0400

    hash

---
+++ hash.c
@@ -0,0 +1,126 @@
+#include "unistd.h"
+#include  "stdlib.h"
+#include  "stdio.h"
+
+
+#define LENGHT 40
+typedef struct node node_t;  
+typedef struct elems{
+	
+	char number[LENGHT];
+	char ascci[LENGHT];
+	struct elems *next;
+}elemes;
+
+
+struct elems * hash_table[LENGHT];
+
+int	ft_strncmp(char *str1, char *str2, unsigned int n)
+{
+	while (n > 0 && *str1 != '\0' && *str2 != '\0')
+	{
+		if (*str1 != *str2)
+			return (*(unsigned char *)str1 - *(unsigned char *) str2);
+		str1++;
+		str2++;
+		n--;
+	}
+	return (0);
+}
+
+
+
+void init_hash_table()
+{
+	int i;
+	i = 0;
+	while(i < LENGHT)
+	{
+		hash_table[i] = NULL;
+		i++;
+	}
+
+}
+
+	unsigned int hash(char *value)
+{
+	unsigned int output;
+	output = 0;
+	while(*value)
+	{
+		output += *value;
+		output = (output * *value) % LENGHT;
+		value++;
+	}
+	return output;
+}
+	void print_table()
+{
+	int i;
+	i = 0;
+	while(i <= LENGHT)
+	{
+		if (hash_table[i] == NULL)
+		{
+		printf("\t%i\t-----\n",i);	
+		}
+		else {
+				struct elems  *tmp= hash_table[i];
+		while(tmp != NULL)
+		{
+			printf("\t%i\t%s\n",i, hash_table[i] -> ascci);	
+			tmp = tmp -> next;
+		}
+		}
+		i++;
+	}
+}
+
+
+	int hash_table_insert(struct elems *p) 
+	{
+	if (p == NULL) 
+		return (0);
+	int index = hash(p->ascci);
+	p->next = hash_table[index];
+	hash_table[index]= p;
+	if (hash_table[index] != NULL)
+		return (0); //to eddit;
+	return (1);
+}
+struct elems *hash_find(char *name)
+{
+unsigned int index = hash(name);
+	elemes *tmp = hash_table[index];
+	while(tmp != NULL && ft_strncmp(tmp->ascci,name,LENGHT)!= 0)
+	{
+		tmp = tmp->next;	
+	}
+	return tmp;
+	if (hash_table[index] != NULL &&
+	ft_strncmp(hash_table[index] -> ascci,name,LENGHT) == 0)
+	return hash_table[index];
+	else
+		return NULL;
+}
+
+int main(void)
+{
+struct elems john = {.number="100",.ascci = "hundred"};
+struct elems johny = {.number="100",.ascci = "reniwgw"};
+init_hash_table();
+hash_table_insert(&john);
+hash_table_insert(&johny);
+struct elems *tmp = hash_find("hundred");
+printf("%shere",tmp->ascci);
+print_table();
+printf("%u\n",hash("10"));
+printf("%u\n",hash("1"));
+printf("%u\n",hash("2"));
+printf("%u\n",hash("3"));
+printf("%u\n",hash("4"));
+printf("%u\n",hash("5"));
+printf("%u\n",hash("6"));
+
+
+}
